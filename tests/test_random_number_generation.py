609fba85beaa8d2f11ca1f6e164e3dde3fa8878d
Dr. Nobel Khandaker <nobel@outlook.com>
2025-06-15 15:32:19 -0500

    Rename class and update project name; modify random seed generation logic

--- tests/test_random_number_generation.py
+++ tests/test_random_number_generation.py
@@ -1,8 +1,11 @@
-from src.generator.random_number_generator import TrueRandomGenerator
+import time
+
+from src.generator.random_number_generator import TrueRandomNumberGenerator
 
 
 def test_random_number_generation():
-    generator = TrueRandomGenerator()
-    for _ in range(100):
+    generator = TrueRandomNumberGenerator()
+    for _ in range(5):
         random = generator.random()
+        time.sleep(1)
         assert random >= generator.CONST_MIN_VAL and random <= generator.CONST_MAX_VAL
